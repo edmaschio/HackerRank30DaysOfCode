16b7da7a3b0053bd27f22dcbc57ab5a33034ea64
edmas <edmas@NB-EDERSON>
2016-05-18 21:47:22 -0300

    Day 21

---
+++ Day_21/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_21
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int[] vInt = new int[] { 1, 2, 3 };
+            string[] vString = new string[] { "Hello", "World" };
+
+            printArray<int>(vInt);
+            printArray<string>(vString);
+        }
+
+        private static void printArray<T>(T[] items)
+        {
+            foreach (var item in items)
+                Console.WriteLine(item);
+        }
+    }
+}
